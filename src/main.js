9f68f57fc94fc0fe6a9949e8798c4184b4ed693f
NastyaSia04 <nastyasia04@gmail.com>
2025-06-16 14:24:48 +0200

    added import boostrap in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './scss/styles.scss'
+import * as bootstrap from 'bootstrap'
 import init from './init'
 
 init()
--- src/validation/validateFeedUrl.js
+++ src/validation/validateFeedUrl.js
@@ -1,22 +1,22 @@
-import * as yup from 'yup';
+import * as yup from 'yup'
 
-export function validateFeedUrl (url, prevUrlList, translate) { 
+export function validateFeedUrl (url, prevUrlList, translate) {
 
-    yup.setLocale({
-        string: {
-            url: translate('errors.url')
-        },
-        mixed: {
-            required: translate('errors.required'),
-            notOneOf: translate('errors.notOneOf')
-        }
-    })
+  yup.setLocale({
+    string: {
+      url: translate('errors.url'),
+    },
+    mixed: {
+      required: translate('errors.required'),
+      notOneOf: translate('errors.notOneOf'),
+    },
+  })
 
-    const validator = yup 
-                        .string()
-                        .url()
-                        .required()
-                        .notOneOf(prevUrlList);
+  const validator = yup
+        .string()
+        .url()
+        .required()
+        .notOneOf(prevUrlList)
 
-    return validator.validate(url);
+  return validator.validate(url)
 }
