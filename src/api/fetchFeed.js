322e1748cdedf0ba7066c01936c1f31bf4264d95
NastyaSia04 <nastyasia04@gmail.com>
2025-06-16 13:37:19 +0200

    edited rules eslint in two files

--- src/api/fetchFeed.js
+++ src/api/fetchFeed.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios from 'axios'
 
-const PROXY_URL = 'https://allorigins.hexlet.app/get';
+const PROXY_URL = 'https://allorigins.hexlet.app/get'
 
 export function fetchFeed(url) {
-    const urlWithoutCache = new URL(PROXY_URL);
-    urlWithoutCache.searchParams.set('url', url);
-    urlWithoutCache.searchParams.set('disable_cache', 'true');
-    urlWithoutCache.searchParams.set('disableCache', 'true');
-    return axios.get(urlWithoutCache.href);
-} 
+  const urlWithoutCache = new URL(PROXY_URL)
+  urlWithoutCache.searchParams.set('url', url)
+  urlWithoutCache.searchParams.set('disable_cache', 'true')
+  urlWithoutCache.searchParams.set('disableCache', 'true')
+  return axios.get(urlWithoutCache.href)
+}
